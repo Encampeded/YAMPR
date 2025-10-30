0ae39efef8fabac2a36a43d42426b05d9540f0d4
encampeded <encampeded@gmail.com>
2025-10-29 21:49:33 -0400

    Convert to lists to nice string representations

--- yampr/song.py
+++ yampr/song.py
@@ -11,12 +11,12 @@ class Song:
         file_path: str
 
         album: str | None
-        album_artist: list[str] | None
+        album_artist: str | None
         track_number: int | None
         disc_number: int | None
-        composer: list[str] | None
-        lyricist: list[str] | None
-        genre: list[str] | None
+        composer: str | None
+        lyricist: str | None
+        genre: str | None
         release_date: str | None
         image: str | None
     """
@@ -26,17 +26,22 @@ class Song:
         for key, value in raw_metadata.items():
             metadata[key.split(':')[1]] = value.value
 
+        def list_get(attribute):
+            """Converts list of strings to string representation if exists."""
+            value = metadata.get(attribute)
+            return None if value is None else ', '.join(value)
+
         self.title = metadata["title"]
-        self.artist = ','.join(metadata["artist"])
+        self.artist = list_get("artist")
         self.length = metadata["length"] / 1000000
         self.file_path = urllib.parse.unquote(metadata["url"])
 
         self.album = metadata.get("album")
-        self.album_artist = metadata.get("albumArtist")
+        self.album_artist = list_get("albumArtist")
         self.track_number = metadata.get("trackNumber")
         self.disc_number = metadata.get("discNumber")
-        self.composer = metadata.get("composer")
-        self.lyricist = metadata.get("lyricist")
-        self.genre = metadata.get("genre")
+        self.composer = list_get("composer")
+        self.lyricist = list_get("lyricist")
+        self.genre = list_get("genre")
         self.release_date = metadata.get("releaseDate")
         self.image = metadata.get("image")
