d736008a6ae37c9dc72392f334ae91a8a911d5f5
encampeded <encampeded@gmail.com>
2025-10-29 21:34:34 -0400

    Moved main loop back to rpc.py

--- yampr/__main__.py
+++ yampr/__main__.py
@@ -1,74 +1,17 @@
-import pypresence
-import time
+from rpc import RichPresence
 import asyncio
-from dbus import DBusConnection
-from image_cache import ImageCache
-from song import Song
-import config
 
 async def main():
+    rpc = RichPresence()
+    try:
+        await rpc.setup()
 
-    #------- Initializing/Setup -------#
+        while True:
+            await rpc.cycle()
 
-    image_cache = ImageCache()
-    rpc = pypresence.AioPresence(
-        client_id = 1427764951690252308,  # this doesn't actually seem necessary. hmmm.
-        loop = asyncio.get_running_loop()
-    )
-
-    dbus_connection = DBusConnection()
-
-    async with asyncio.TaskGroup() as tg:
-        tg.create_task(dbus_connection.setup())
-        tg.create_task(rpc.connect())
-        if config.VERIFY_IMAGES:
-            tg.create_task(image_cache.verify_images())
-
-
-    while True: # ------- Main Loop -------#
-
-        try:
-            print("Waiting for player...")
-            await dbus_connection.find_player()
-            print("Found!")
-
-            while dbus_connection.player_playing:
-
-                print("Getting info...")
-                async with asyncio.TaskGroup() as tg:
-                    get_song = tg.create_task(dbus_connection.get_song())
-                    get_position = tg.create_task(dbus_connection.get_position())
-
-                song = get_song.result()
-                position = get_position.result()
-
-                def try_get(value: str) -> str:
-                    return getattr(song, value, value)
-
-                print("Updating and waiting...")
-                async with asyncio.TaskGroup() as tg:
-
-                    tg.create_task(rpc.update(
-                        activity_type=pypresence.ActivityType.LISTENING,
-                        name=try_get(config.LISTENING_TO),
-
-                        details=try_get(config.TITLE),
-                        state=try_get(config.SUBTITLE),
-
-                        start=round(time.time() - position),
-                        end=round(time.time() - position + song.length),
-
-                        large_image= await image_cache.get(song),
-                        large_text=try_get(config.IMAGE_LABEL)
-                    ))
-
-                    tg.create_task(dbus_connection.properties_change.wait())
-                print("Properties Changed!")
-
-        finally:
-            await dbus_connection.close()
-            #asyncio.get_running_loop().close()
-            rpc.close()
+    finally:
+        rpc.takedown()
+        asyncio.get_running_loop().close()
 
 if __name__ == "__main__":
     asyncio.run(main())
---
+++ yampr/rpc.py
@@ -0,0 +1,84 @@
+import pypresence
+import time
+from song import Song
+from image_cache import ImageCache
+from dbus import DBusConnection
+import asyncio
+import config
+
+class RichPresence:
+
+    def __init__(self):
+        self._image_cache = ImageCache()
+        self._rpc = pypresence.AioPresence(
+            client_id = 1427764951690252308,  # this doesn't actually seem necessary. hmmm.
+            loop = asyncio.get_running_loop()
+        )
+        self._dbus_connection = DBusConnection()
+
+        self.song: Song | None = None
+        self.position = 0.0
+        self.image = ""
+
+
+    async def setup(self):
+        async with asyncio.TaskGroup() as tg:
+            tg.create_task(self._dbus_connection.setup())
+            tg.create_task(self._rpc.connect())
+            if config.VERIFY_IMAGES:
+                tg.create_task(self._image_cache.verify_images())
+
+    async def get_song(self):
+        self.song = await self._dbus_connection.get_song()
+        self.image = await self._image_cache.get(self.song)
+
+    async def get_position(self):
+        self.position = await self._dbus_connection.get_position()
+
+    async def update(self):
+
+        def try_get(value: str):
+            return getattr(self.song, value, value)
+
+        await self._rpc.update(
+            activity_type=pypresence.ActivityType.LISTENING,
+            name=try_get(config.LISTENING_TO),
+
+            details=try_get(config.TITLE),
+            state=try_get(config.SUBTITLE),
+
+            start=round(time.time() - self.position),
+            end=round(time.time() - self.position + self.song.length),
+
+            large_image=self.image,
+            large_text=try_get(config.IMAGE_LABEL)
+        )
+
+    async def cycle(self):
+        print("\nFinding Player...")
+        await self._dbus_connection.find_player()
+
+        while self._dbus_connection.player_playing:
+
+            print("Getting info...")
+            async with asyncio.TaskGroup() as tg:
+                # SHUT THE FUCK UP PYCHARM/
+                # noinspection PyUnresolvedReferences
+                tg.create_task(self.get_song())
+                tg.create_task(self.get_position())
+
+            print("Updating and awaiting properties change...")
+            async with asyncio.TaskGroup() as tg:
+                tg.create_task(self.update())
+                tg.create_task(self._dbus_connection.properties_change.wait())
+
+            print("Properties changed!")
+            await self._rpc.clear()
+            self._dbus_connection.properties_change.clear()
+
+    def takedown(self):
+        self._dbus_connection.close()
+        self._rpc.close()
+
+
+
