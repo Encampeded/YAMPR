4725b18e38601a4b7c0f4439bc54622752e2e75d
encampeded <encampeded@gmail.com>
2025-11-05 17:28:11 -0500

    remove testing raise

--- yampr/mpris_dbus.py
+++ yampr/mpris_dbus.py
@@ -49,12 +49,8 @@ class DBusConnection:
     def _set_song(self, metadata: dict):
         self.song.update_from_metadata(metadata)
 
-    def _update_song(self, _, changed_properties: dict, other):
+    def _update_song(self, _, changed_properties: dict, __):
         print("Received PropertiesChanged!")
-
-        if len(other) > 0:
-            print(other)
-            raise("wowzers")
 
         if "PlaybackStatus" in changed_properties:
             self.player_playing = False
